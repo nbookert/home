4bc6e911e5e026876123651d95b67b5ba947a137
nbookert <59941515+nbookert@users.noreply.github.com>
2022-10-17 19:23:01 -0400

    updating photo size

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -63,7 +63,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-          <img src={siteConfig.profilePhoto} alt="My Profile Photo"/>
+          <img src={siteConfig.profilePhoto} alt="My Profile Photo" width={50%} height=50%/>
           </PromoSection>
         </div>
       </SplashContainer>
