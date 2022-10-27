790a334c4ef46c52d87eea674b0cf4b04234d412
Nyteisha Bookert <nyteishabookert@Nyteishas-MBP.triad.rr.com>
2022-10-27 18:35:03 -0400

    Reset to working mode

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build'}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/tech-portfolio'}
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${GITHUB_TOKEN}" ] && {
