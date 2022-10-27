33188df2f28ff0c21a127a142d4a1371e832b81a
nbookert <59941515+nbookert@users.noreply.github.com>
2022-10-27 15:27:28 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const siteConfig = {
     // My research investigates the security and privacy of emerging technologies to protect end-users. 
     profilePhoto: 'img/profile.jpg',
     url: 'https://nbookert.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    //baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
